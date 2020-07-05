1afd2fc78af4302176eb7d0c1820721979015bb7
Jan Filip <jasiufilip@gmail.com>
2020-07-05 16:06:37 +0200

    preprocessing

--- ecg_ml.py
+++ ecg_ml.py
@@ -6,7 +6,7 @@ import glob
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-
+import matplotlib.image as mpimg
 """## Utils"""
 
 path='D:\Jasiu\Dokumenty\studia\inzynierka\project\qt-database-1.0.0\\'
@@ -43,7 +43,7 @@ def SymbolsOneHotEncoding(path):
     signals, fields = wfdb.rdsamp(path, sampfrom=sampfrom, sampto=sampto, channels=[0])
 
     annotation = wfdb.rdann(path, extention, sampfrom=sampfrom, sampto=sampto)
-   # print(fields['fs'])
+    # print(fields['fs'])
     d = {'symbol': annotation.symbol}
     df = pd.DataFrame(d, (annotation.sample-sampfrom))
     sym = df['symbol']
@@ -74,81 +74,99 @@ def Segmentation(records):
 
 """## Main"""
 
-data=Segmentation(records[:20]) 
-#print(data[0][1])
-# for i in range(0, len(data)):
-#     plotecg(data[i][0],data[i][1])
-#     plt.close()
+data=Segmentation(records[:41]) 
+    
+def smooth(x,window_len=11,window='hanning'):
+    x = x.T[0]
+    if x.ndim != 1:
+        raise ValueError #"smooth only accepts 1 dimension arrays."
 
-# x = list()
-# for i in range(len(data)):
-#     x.append(data[i][0])
-# y = list()
-# for i in range(len(data)):
-#     y.append(data[i][1].reset_index())
-      
+    if x.size < window_len:
+        raise ValueError #"Input vector needs to be bigger than window size."
+
+
+    if window_len<3:
+        return x
+
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError # "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+
+    s=np.r_[x[window_len-1:0:-1],x,x[-2:-window_len-1:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=np.ones(window_len,'d')
+    else:
+        w=eval('np.'+window+'(window_len)')
+
+    y=np.convolve(w/w.sum(),s,mode='valid')
+    return y
+
+for i in data:
+    i = (smooth(i[0]), i[1])
+
+for i in range(0, len(data)):
+    plotecg(data[i][0],data[i][1])
+    plt.close()
+    
 def fragmentation(data):
-    P = []
-    QRS=[]
-    T=[]
-    booler =  False
+    signals = []
+    booler = True
+    stop=0
+    start=0
+    indeStop=0
+    indeStart=0
+    
     for i in data:
-        signal = i[0]
-        features = i[1].reset_index()
-        for  index, symbol in zip(features['index'], features['symbol']):
-            if booler ==True:
-                stop = index 
-                booler =False
-                if previousSymbol == 'p':
-                    P.append(signal[start:stop])
-                if previousSymbol == 'N':
-                    
-                    QRS.append(signal[start:stop])
-                if previousSymbol == 't':
-                    T.append(signal[start:stop])
-                    
-            if symbol == 'p':
-                start = previousIndex
-                booler =True
-            if symbol == 'N':
-                start = previousIndex
-                booler =True
-            if symbol == 't':
-                start = previousIndex
-                booler =True
-            previousIndex=index
-            previousSymbol = symbol
+         signal = i[0]
+         features = i[1].reset_index()
+         for  inde, index, symbol in zip(features.index, features['index'], features['symbol']):
+             
+             if (symbol == 'p' and booler == False):
+                 indeStop = previousInde
+                 stop = previousIndex
+                 if(start<stop):
+                     signals.append((signal[start:stop],features[indeStart:indeStop]))
+                 booler=True
+             if (symbol == 'p' and booler==True):
+                 start = previousIndex
+                 indeStart = previousInde
+                 booler =False
+             
+             previousInde=inde
+             previousIndex=index
+             previousSymbol = symbol
 
-    return [P,QRS,T]
+    return signals        
 
-waves= fragmentation(data)
+signals=fragmentation(data)
 
+for i in range(len(signals)-1, 0 , -1):
+    if signals[i][0].shape[0] >2000:
+        signals.pop(i)
+        
+def EcgToImages(signals,path):
+    y=0
+    for i in signals:
+       fig = plt.figure()
+       plt.ylim(min(i[0]), max(i[0]))
+       plt.xlim(0, signals[y][0].shape[0])
+       plt.plot(i[0])
+       plt.axis('off')
+       fig.savefig(path+'\{}.png'.format(y))
+       plt.close(fig)       
+       y=y+1
 
-def images(waves):   
-    path='D:\\Jasiu\\Dokumenty\\studia\\inzynierka\\project\\images\\'     
-    j=0
-    for i in waves[0]:
-        fig = plt.figure()
-        plt.plot(i)
-        plt.axis('off')
-        plt.savefig(path+'P\{}.png'.format(j))
-        plt.close(fig)
-        j=j+1
-    j=0
-    for i in waves[1]:
-        fig = plt.figure()
-        plt.plot(i)
-        plt.axis('off')
-        plt.savefig(path+'QRS\{}.png'.format(j))
-        plt.close(fig)
-        j=j+1
-    j=0
-    for i in waves[2]:
-        fig = plt.figure()
-        plt.plot(i)
-        plt.axis('off')
-        plt.savefig(path+'T\{}.png'.format(j))
-        plt.close(fig)
-        j=j+1
-images(waves)
-print(j)
+path='D:\Jasiu\Dokumenty\studia\inzynierka\project\images'
+# EcgToImages(signals,path)
+
+def readImages():
+    img = []
+    images=glob.glob(path+"\*.png")
+    for i in images: 
+        img.append(mpimg.imread(i))
+    return img
+
+images = readImages()
+
